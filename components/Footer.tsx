8a71d5eb49033f364aeabc0e4b68f1a75f34d440
shrey0801 <shrey.0801@gmail.com>
2024-06-30 03:22:33 -0700

    commit2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React from 'react'
-import MagicButton from './ui/MagicButton'
-import { FaLocationArrow } from 'react-icons/fa'
-import { socialMedia } from '@/data'
-import { RxResume } from 'react-icons/rx'
+import React from 'react';
+import Image from 'next/image'; // Import next/image for optimized images
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
+import { socialMedia } from '@/data';
+import { RxResume } from 'react-icons/rx';
 
 const Footer = () => {
   return (
@@ -15,7 +16,7 @@ const Footer = () => {
                 Ready to take <span className='text-purple'>your</span> digital presence to next level?
             </h1>
             <p className='text-white-200 md:mt-10 my-5 text-center'>
-                Reach out to me today and let's discuss how I can help you achieve your goal.
+                Reach out to me today and let&apos;s discuss how I can help you achieve your goal.
             </p>
             <a href='mailto:sthakka9@asu.edu'>
                 <MagicButton 
@@ -38,13 +39,13 @@ const Footer = () => {
             <div className='flex items-center md:gap-3 gap-6'>
                 {socialMedia.map((profile) =>
                     <a key={profile.id} href={profile.link} target="_blank" rel="noopener noreferrer" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                        <img src={profile.img} alt={`profile-${profile.id}`} width={20} height={20} />
+                        <Image src={profile.img} alt={`profile-${profile.id}`} width={20} height={20} /> {/* Replace <img> with <Image /> */}
                     </a>
                 )}
             </div>
         </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
